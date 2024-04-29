6a260521fc0199c3468d281776aede0dc1a8c067
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2024-04-29 16:19:18 +0100

    merkletree implementation understood

--- client/index.js
+++ client/index.js
@@ -4,11 +4,18 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const merkleTree = new MerkleTree(niceList);
+
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const index = Math.floor(Math.random() * niceList.length);
+  const name = niceList[index];
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name,
+    proof,
   });
 
   console.log({ gift });
